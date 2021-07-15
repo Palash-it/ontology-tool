e7cf7bb2862ff7a0421e16c35ac8b75b583c6340
palash_fortunatis <palash@fortunatis.ch>
2021-07-15 21:42:38 +0600

    Spring boot app initialized and ensured test running properly

---
+++ src/main/java/com/ebispot/palash/ontologyTools/OntologyToolsApplication.java
@@ -0,0 +1,13 @@
+package com.ebispot.palash.ontologyTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntologyToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntologyToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebispot/palash/ontologyTools/OntologyToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebispot.palash.ontologyTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntologyToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
