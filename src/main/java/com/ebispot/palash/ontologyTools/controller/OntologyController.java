d1d3e963606217d8a6ecc172be383c870bdf7205
palash_fortunatis <palash@fortunatis.ch>
2021-07-18 15:51:47 +0600

    added CORS

--- src/main/java/com/ebispot/palash/ontologyTools/controller/OntologyController.java
+++ src/main/java/com/ebispot/palash/ontologyTools/controller/OntologyController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.ebispot.palash.ontologyTools.exception.ResourceNotFoundException;
 import com.ebispot.palash.ontologyTools.model.OntologyModel;
 import com.ebispot.palash.ontologyTools.service.OntologyService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/ols/api")
 public class OntologyController {
